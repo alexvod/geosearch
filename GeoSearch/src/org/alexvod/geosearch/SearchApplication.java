ebc219ea50c88d14eee48f1f5c2d2f479f35cd49
Alexander Vodomerov <alexander.vodomerov@gmail.com>
2010-08-23 23:52:34 +0400

    Rewrite with Application object instead of static members.

---
+++ GeoSearch/src/org/alexvod/geosearch/SearchApplication.java
@@ -0,0 +1,37 @@
+package org.alexvod.geosearch;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class SearchApplication extends Application {
+  private static final String LOGTAG = "SearchApplication";
+  private Searcher searcher;
+  
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    createSearcher();
+  }
+
+  public Searcher createSearcher() {
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    String searchMode = prefs.getString("search_mode", "remote");
+    if (searchMode.equals("remote")) {
+      Log.e(LOGTAG, "Creating new RemoteSearcher");
+      searcher = new RemoteSearcher();
+    } else if (searchMode.equals("local")) {
+      Log.e(LOGTAG, "Creating new LocalSearcher");
+      searcher = new LocalSearcher();
+    } else {
+      throw new RuntimeException("Unknown search mode " + searchMode);
+    }
+    searcher.loadPreferences(prefs);
+    return searcher;
+  }
+  
+  public Searcher getSearcher() {
+    return searcher;
+  }
+}
